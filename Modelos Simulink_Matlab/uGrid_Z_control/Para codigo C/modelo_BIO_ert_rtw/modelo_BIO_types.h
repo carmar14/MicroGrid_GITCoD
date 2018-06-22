1b1c4fae53f5de1b808f271db23cbfa448ca4b48
Mario <carmar141414@hotmail.com>
2018-06-22 11:33:44 -0500

    Trabajo sobre emulación

---
+++ Modelos Simulink_Matlab/uGrid_Z_control/Para codigo C/modelo_BIO_ert_rtw/modelo_BIO_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: modelo_BIO_types.h
+ *
+ * Code generated for Simulink model 'modelo_BIO'.
+ *
+ * Model version                  : 1.13
+ * Simulink Coder version         : 8.12 (R2017a) 16-Feb-2017
+ * C/C++ source code generated on : Fri Jun 22 10:35:01 2018
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_modelo_BIO_types_h_
+#define RTW_HEADER_modelo_BIO_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_modelo_BIO_T RT_MODEL_modelo_BIO_T;
+
+#endif                                 /* RTW_HEADER_modelo_BIO_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
